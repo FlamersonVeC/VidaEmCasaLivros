7d3fd9c08bf749d054b55ff88d50d84ca33acb98
Flamerson Bezerra <Flamerson.Bezerra@vidaemcasa.local>
2024-09-30 16:15:37 -0300

    tentando corrigir o erro no prisma

--- app/api/allBooks/[bookId]/route.js
+++ app/api/allBooks/[bookId]/route.js
@@ -1,26 +1,27 @@
-import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 
-export async function GET(req , {params} ){
+export async function GET(request, { params }) {
+  const { bookId } = params;
 
-    const { bookId } = params;
+  try {
+    const book = await prisma.book.findUnique({
+      where: { id: Number(bookId) },
+    });
 
-    try{
-
-        const book = await prisma.book.findUnique({
-            where: {id: Number(bookId)}
-        })
-
-        if(!book){
-            return NextResponse.json({message: "Livro não encontrado!"}, {status: 404})
-        }
-
-        return NextResponse.json(book);
-
-    }catch(err){
-        console.error(error);
-        return NextResponse.json({ message: "Erro ao buscar o livro" }, { status: 500 });
+    if (!book) {
+      return new Response(JSON.stringify({ message: 'Livro não encontrado' }), {
+        status: 404,
+      });
     }
 
+    return new Response(JSON.stringify(book), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Erro ao buscar o livro' }), {
+      status: 500,
+    });
+  }
 }
